5a9662eb659060d6ed94805b9d5a2ec694642627
yuebaix <yuebaix@outlook.com>
2021-10-06 06:16:37 +0800

    😉 add basic auth for feign

---
+++ app/jiuzhou-bizfacade/src/main/java/com/yuebaix/jiuzhou/app/bizfacade/FeignConfig.java
@@ -0,0 +1,19 @@
+package com.yuebaix.jiuzhou.app.bizfacade;
+
+import feign.auth.BasicAuthRequestInterceptor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FeignConfig {
+    @Value("${spring.security.user.name}")
+    private String username;
+    @Value("${spring.security.user.password}")
+    private String password;
+
+    @Bean
+    public BasicAuthRequestInterceptor basicAuthRequestInterceptor() {
+        return new BasicAuthRequestInterceptor(username, password);
+    }
+}
