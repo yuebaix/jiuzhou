5f86f60f66baf91bd472de8b44e779b5ebb52617
yuebaix <yuebaix@outlook.com>
2021-10-05 17:31:38 +0800

    😊 add SwaggerResourcesProvider implementation. ie, GatewayRoutesOasSwaggerResourcesProvider

---
+++ sys/jiuzhou-gate/src/main/java/com/yuebaix/jiuzhou/sys/gate/config/GatewayRoutesOasSwaggerResourcesProvider.java
@@ -0,0 +1,56 @@
+package com.yuebaix.jiuzhou.sys.gate.config;
+
+import org.springframework.cloud.gateway.config.GatewayProperties;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.support.NameUtils;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.env.Environment;
+import springfox.documentation.spring.web.DocumentationCache;
+import springfox.documentation.spring.web.plugins.DocumentationPluginsManager;
+import springfox.documentation.swagger.web.InMemorySwaggerResourcesProvider;
+import springfox.documentation.swagger.web.SwaggerResource;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Primary
+@Configuration
+public class GatewayRoutesOasSwaggerResourcesProvider extends InMemorySwaggerResourcesProvider {
+    protected static final String API_DOCS_URI = "/v3/api-docs";
+    private static final String SWAGGER_VERSION = "3.0";
+    private final RouteLocator routeLocator;
+    private final GatewayProperties gatewayProperties;
+
+    public GatewayRoutesOasSwaggerResourcesProvider(
+            RouteLocator routeLocator, GatewayProperties gatewayProperties,
+            Environment environment, DocumentationCache documentationCache, DocumentationPluginsManager pluginsManager) {
+        super(environment, documentationCache, pluginsManager);
+        this.routeLocator = routeLocator;
+        this.gatewayProperties = gatewayProperties;
+    }
+
+    @Override
+    public List<SwaggerResource> get() {
+        List<SwaggerResource> resources = new ArrayList<>();
+        resources.addAll(super.get());
+        List<String> routes = new ArrayList<>();
+        routeLocator.getRoutes().subscribe(route -> routes.add(route.getId()));
+        gatewayProperties.getRoutes().stream().filter(routeDefinition -> routes.contains(routeDefinition.getId())).forEach(route -> {
+            route.getPredicates().stream()
+                    .filter(predicateDefinition -> ("Path").equalsIgnoreCase(predicateDefinition.getName()))
+                    .forEach(predicateDefinition -> resources.add(swaggerResource(route.getId(),
+                            predicateDefinition.getArgs().get(NameUtils.GENERATED_NAME_PREFIX + "0")
+                                    .replace("**", API_DOCS_URI))));
+        });
+        return resources;
+    }
+
+    private SwaggerResource swaggerResource(String name, String location) {
+        SwaggerResource swaggerResource = new SwaggerResource();
+        swaggerResource.setName(name);
+        swaggerResource.setLocation(location);
+        swaggerResource.setSwaggerVersion(SWAGGER_VERSION);
+        return swaggerResource;
+    }
+}
