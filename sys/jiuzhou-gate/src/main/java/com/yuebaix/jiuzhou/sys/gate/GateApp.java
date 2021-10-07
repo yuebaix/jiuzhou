1745e21c618a99cd4001dd1e4f6b220f9085c1bf
yuebaix <yuebaix@outlook.com>
2021-10-08 01:46:16 +0800

    😉 update gate

--- sys/jiuzhou-gate/src/main/java/com/yuebaix/jiuzhou/sys/gate/GateApp.java
+++ sys/jiuzhou-gate/src/main/java/com/yuebaix/jiuzhou/sys/gate/GateApp.java
@@ -3,7 +3,9 @@ package com.yuebaix.jiuzhou.sys.gate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 
+@EnableWebFluxSecurity
 @EnableDiscoveryClient
 @SpringBootApplication
 public class GateApp {
