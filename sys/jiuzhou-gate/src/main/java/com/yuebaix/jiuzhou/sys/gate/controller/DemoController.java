ba25ed0b7f31d214c8d4e8a400a78a6ce49fdf41
yuebaix <yuebaix@outlook.com>
2021-10-09 03:50:06 +0800

    😉 add way to fetch ServerWebExchange

--- sys/jiuzhou-gate/src/main/java/com/yuebaix/jiuzhou/sys/gate/controller/DemoController.java
+++ sys/jiuzhou-gate/src/main/java/com/yuebaix/jiuzhou/sys/gate/controller/DemoController.java
@@ -4,9 +4,13 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.filter.reactive.ServerWebExchangeContextFilter;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
 
 @Api(tags = "示例接口")
 @Slf4j
@@ -16,9 +20,19 @@ public class DemoController {
     @Value("${spring.application.name}")
     private String appName;
 
+    @Bean
+    public ServerWebExchangeContextFilter serverWebExchangeContextFilter() {
+        return new ServerWebExchangeContextFilter();
+    }
+
     @ApiOperation("服务名称")
     @GetMapping("/appName")
-    public String appName() {
-        return appName;
+    public Mono<String> appName() {
+        return Mono.subscriberContext().map(ctx -> {
+            ServerWebExchange exchange = ServerWebExchangeContextFilter.get(ctx).get();
+            String authorization = exchange.getRequest().getHeaders().getFirst("Authorization");
+            log.info(authorization);
+            return appName;
+        });
     }
 }
